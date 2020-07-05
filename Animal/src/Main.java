0d6407581f9b06ba1ed59a43547f7a0ecf075eca
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-05 22:11:23 +0300

    Update Main.java

--- Animal/src/Main.java
+++ Animal/src/Main.java
@@ -2,15 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         Animal animals = new Animal();
-        Cat cat = new Cat("Мурзик", 100, 0);
+        Cat cat = new Cat("Мурзик", 100, 0, 1);
         animals.animalInfo();
         cat.catInfo();
-        Dog dog = new Dog("Бобик", 125, 5);
+        Dog dog = new Dog("Бобик", 125, 5, 0.2);
         animals.animalInfo();
         dog.dogInfo();
 
     }
 }
-
-
 
