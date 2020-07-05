dc4ebbccd423216ddb878225163e8345ae36747b
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-05 22:12:48 +0300

    Update Dog.java

--- Animal/src/Dog.java
+++ Animal/src/Dog.java
@@ -1,16 +1,18 @@
 public class Dog extends Animal {
-    public Dog(String name, int run, int swim) {
-        super();
+    public Dog(String name, int run, int swim, double jump) {
         this.name = name;
         this.run = run;
         this.swim = swim;
+        this.jump = jump;
+
 
     }
 
     public void dogInfo() {
-        if (run >= 200 || swim >= 10) {
+        if (run >= 200 || swim >= 10 || jump >= 0.5) {
             return;
         }
-        System.out.println("Собака " + name + " пробежал " + run + " метров" + " проплыл " + swim + " метров");
+        System.out.println("Собака " + name + "\n пробежал " + run + " метров" + "\n проплыл " + swim + " метров" + "\n прыгнул вверх на " + jump + " метр ");
     }
 }
+
