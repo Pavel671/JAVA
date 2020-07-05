f20b505f63a06f9e3517f4b10ca14233f7053e72
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-05 22:13:21 +0300

    Update Cat.java

--- Animal/src/Cat.java
+++ Animal/src/Cat.java
@@ -1,17 +1,15 @@
 public class Cat extends Animal {
 
-    public Cat(String name, int run, int swim) {
+    public Cat(String name, int run, int swim, int jump) {
         this.name = name;
         this.run = run;
         this.swim = swim;
+        this.jump = jump;
     }
 
 
     public void catInfo() {
-        if (run >= 200 || swim > 0) return;
-        System.out.println("Кот " + name + " Пробежал " + run + " метров"+ "");
+        if (run >= 200 || swim > 0 || jump >= 2) return;
+        System.out.println("Кот " + name + "\nПробежал " + run + " метров"+ "\n прыгнул вверх на " + jump + " метр ");
     }
 }
-
-
-
