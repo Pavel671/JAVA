84200474ea7402b300ec19bb31cf2df04a7f4407
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-05 22:10:30 +0300

    Update and rename Animal/src/Animal.java to животное/ЦСИ/Animal.java

--- животное/ЦСИ/Animal.java
+++ животное/ЦСИ/Animal.java
@@ -2,6 +2,7 @@ public class Animal {
     protected String name;
     protected int run;
     protected int swim;
+    protected double jump;
 
 
     public Animal(String name) {
@@ -15,8 +16,6 @@ public class Animal {
     public void animalInfo() {
         System.out.println("Животное");
     }
-
-
 
 
 }
