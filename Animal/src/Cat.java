8f7aaecb2be07a589a39e7b634d94eb95cfaaab5
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-07 02:17:52 +0300

    Update Cat.java

--- Animal/src/Cat.java
+++ Animal/src/Cat.java
@@ -1,15 +1,31 @@
 public class Cat extends Animal {
+    private final int run = 200;
+    private final double jump = 2.0;
 
-    public Cat(String name, int run, int swim, int jump) {
-        this.name = name;
-        this.run = run;
-        this.swim = swim;
-        this.jump = jump;
+
+    @Override
+    public void run(int length) {
+        if ((length >= 0) && (length <= run)) System.out.println("cat.run: true");
+        else System.out.println("cat.run: false");
     }
 
+    @Override
+    public void swim(int length) {
+        System.out.println("Cats usually do not want to swim");
+    }
 
-    public void catInfo() {
-        if (run >= 200 || swim > 0 || jump >= 2) return;
-        System.out.println("Кот " + name + "\nПробежал " + run + " метров"+ "\n прыгнул вверх на " + jump + " метр ");
+    @Override
+    public void jump(double height) {
+        if ((height >= 0) && (height <= jump)) System.out.println("cat.jump: true");
+        else System.out.println("cat.jump: fase");
+    }
+
+    @Override
+    public void info() {
+        System.out.println();
     }
 }
+
+
+
+
