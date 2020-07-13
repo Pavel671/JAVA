c98ae471ad69a2aec17ee8442197ce1f82c18c0e
Pavel671 <66891641+Pavel671@users.noreply.github.com>
2020-07-13 23:25:12 +0300

    Update Main.java

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,12 +1,5 @@
-import javax.swing.*;
-
-
 class Main {
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        calc.pack();
-        calc.setLocationRelativeTo(null);
-        calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calc.setVisible(true);
+        new Calc();
     }
 }
