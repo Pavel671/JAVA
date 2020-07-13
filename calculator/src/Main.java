3acdaa14f67164d9459ab4b659ecb44df39b6bfc
Pavel Gavrilov <pavel671@bk.ru>
2020-07-13 19:43:32 +0300

    Pavel Gavrilov pavel671@bk.ru

---
+++ calculator/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Main {
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        calc.pack();
+        calc.setLocationRelativeTo(null);
+        calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calc.setVisible(true);
+    }
+}
