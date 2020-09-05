eabd36d735a3508a1c7d8a3fa5b47fd775822fbc
Pavel Gavrilov <pavel671@bk.ru>
2020-09-05 22:13:21 +0300

    Pavel Gavrilov pavel671@bk.ru

--- Java3.lesson3/src/main/java/lesson2/client/Main.java
+++ Java3.lesson3/src/main/java/lesson2/client/Main.java
@@ -1,4 +1,4 @@
-package client;
+package lesson2.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-
-public class RunClient extends Application {
-
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
-        primaryStage.setTitle("Simple network chat");
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        primaryStage.setTitle("BrainsChat Client");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
