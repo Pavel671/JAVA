3035769d43befc20521d6a5eb8610380303a4521
Pavel Gavrilov <pavel671@bk.ru>
2020-09-05 20:24:50 +0300

    Pavel Gavrilov pavel671@bk.ru

--- Java3.lesson3/src/main/java/lesson3/client/Main.java
+++ Java3.lesson3/src/main/java/lesson3/client/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/lesson3/main.fxml"));
-        primaryStage.setTitle("BrainsChat Client");
+        primaryStage.setTitle("Chat ");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
